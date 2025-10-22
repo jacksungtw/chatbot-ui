bb2cd0e68d49b5e62638f67fdb60a35ce771e37c
jacksungtw <jack_sung_tw@yahoo.com.tw>
2025-10-22 20:14:54 +0800

    Change runtime from edge to nodejs
    xport const runtime = "nodejs"
    export const maxDuration = 60

--- app/api/assistants/openai/route.ts
+++ app/api/assistants/openai/route.ts
@@ -2,7 +2,8 @@ import { checkApiKey, getServerProfile } from "@/lib/server/server-chat-helpers"
 import { ServerRuntime } from "next"
 import OpenAI from "openai"
 
-export const runtime: ServerRuntime = "edge"
+xport const runtime = "nodejs"
+export const maxDuration = 60
 
 export async function GET() {
   try {
